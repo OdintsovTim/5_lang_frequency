c6a6f738634a5406d5587739e4bfe591b583c32a
Odintsov Timur <timodin@yandex.ru>
2019-08-31 14:31:59 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,39 @@
+import argparse
+from collections import Counter
+import re
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file_handler:
+        return file_handler.read()
 
 
-def get_most_frequent_words(text):
-    pass
+def get_words_from_text(text):
+    text_without_digits = re.sub(r'\d+', '', text_file)
+    words_without_punctuation = re.findall(r'\w+', text_without_digits)
+    return words_without_punctuation
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath')
+    args = parser.parse_args()
+    return args
+
+
+def get_most_frequent_words(words):
+    counted_words = Counter(words)
+    return counted_words.most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        filepath = create_parser().filepath
+        text_file = load_data(filepath)
+        words = get_words_from_text(text_file)
+        counted_words = get_most_frequent_words(words)
+        print(counted_words)
+    except FileNotFoundError:
+        print('Такого файла не существует')
+    except UnicodeDecodeError:
+        print('Это не текстовый файл')
